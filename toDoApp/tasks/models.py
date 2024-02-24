8a7a46b174c05bab4006f9363f7d37caeb8d4fc6
typicaleoxx <lsneha991@gmail.com>
2024-02-24 17:46:19 +0545

    updated models(status, default)

--- toDoApp/tasks/models.py
+++ toDoApp/tasks/models.py
@@ -10,7 +10,7 @@ class Task(models.Model):
     )
     title = models.CharField(max_length=200)
     description = models.TextField(default=None)
-    status = models.CharField(max_length=20, choices=CHOICES)
+    status = models.CharField(max_length=20, choices=CHOICES, default="NotStarted")
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
